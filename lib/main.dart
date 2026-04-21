5d666cd1c79abca1eae397948f86f5945a7de2e9
CounterApp <277802546+taufiq-codebase@users.noreply.github.com>
2026-04-21 22:38:59 +0600

    text updated

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.arrow_upward),
       ),
     );
   }
